f6389604d019770de87af3d2b55b88e8b80c6126
FredNekrasov <frednekrasov49@gmail,com>
2024-07-22 17:48:52 +0300

    import optimization in the Theme.kt

--- app/src/main/java/com/fredprojects/helloworld/ui/theme/Theme.kt
+++ app/src/main/java/com/fredprojects/helloworld/ui/theme/Theme.kt
@@ -1,13 +1,8 @@
 package com.fredprojects.helloworld.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
@@ -39,11 +34,9 @@ fun HelloWorldTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
-
     MaterialTheme(
         colorScheme = colorScheme, typography = Typography, content = content
     )
