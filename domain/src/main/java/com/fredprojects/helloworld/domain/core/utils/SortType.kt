04c25cc45558ed0a2db113e31639476390573056
FredNekrasov <frednekrasov49@gmail,com>
2024-07-22 18:55:13 +0300

    creating a class (SortType) for sorting data

---
+++ domain/src/main/java/com/fredprojects/helloworld/domain/core/utils/SortType.kt
@@ -0,0 +1,11 @@
+package com.fredprojects.helloworld.domain.core.utils
+
+/**
+ * Sort type
+ * @property Ascending if the data should be sorted in ascending order
+ * @property Descending if the data should be sorted in descending order
+ */
+sealed class SortType {
+    data object Ascending : SortType()
+    data object Descending : SortType()
+}
