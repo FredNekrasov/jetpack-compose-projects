e9204eebae31d93699bd3cc64e60f19ececabe82
FredNekrasov <frednekrasov49@gmail,com>
2024-07-23 00:16:00 +0300

    creating API services to get the data from the API. (IAQService, IAstronomyInfoService, IMathService)

---
+++ data/src/main/java/com/fredprojects/helloworld/data/remote/services/IAstronomyInfoService.kt
@@ -0,0 +1,28 @@
+package com.fredprojects.helloworld.data.remote.services
+
+import com.fredprojects.helloworld.data.remote.dto.AstronomyInfoDto
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * IAstronomyInfoService represents the interface for the astronomy info service.
+ * The astronomy info service is used to get data from the server.
+ */
+interface IAstronomyInfoService {
+    /**
+     * getAstronomyInfo is used to get data from the server.
+     * @param ra is the right ascension of the location
+     * @param dec is the declination of the location
+     * @param radius is the radius of the search
+     * @return list of [AstronomyInfoDto]
+     */
+    @GET("/catalog/")
+    suspend fun getAstronomyInfo(
+        @Query("ra") ra: String,
+        @Query("dec") dec: String,
+        @Query("radius") radius: Float
+    ): Map<String, AstronomyInfoDto>?
+    companion object {
+        const val BASE_URL = "https://api.astrocats.space"
+    }
+}
---
+++ data/src/main/java/com/fredprojects/helloworld/data/remote/services/IMathService.kt
@@ -0,0 +1,22 @@
+package com.fredprojects.helloworld.data.remote.services
+
+import com.fredprojects.helloworld.domain.features.clients.common.MathModel
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+/**
+ * IMathService is used to work with the Math API.
+ */
+interface IMathService {
+    /**
+     * GetResult is used to get the result of the expression.
+     * @param expression the expression to be solved
+     * @return the result of the expression
+     */
+    @GET("/api/v2/simplify/{expression}")
+    fun getResult(@Path("expression") expression: String): Call<MathModel>
+    companion object {
+        const val BASE_URL = "https://newton.now.sh/"
+    }
+}
