e57eb5e14026a1b73acb44cc8cd9f984c27c406f
FredNekrasov <frednekrasov49@gmail,com>
2024-07-24 19:55:27 +0300

    creating an AnimeQuotesVM to provide anime quotes in the UI

---
+++ app/src/main/java/com/fredprojects/helloworld/presentation/features/clients/anime/vm/AnimeQuotesVM.kt
@@ -0,0 +1,27 @@
+package com.fredprojects.helloworld.presentation.features.clients.anime.vm
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.fredprojects.helloworld.domain.core.repositories.IClientRepository
+import com.fredprojects.helloworld.domain.core.utils.ConnectionStatus
+import com.fredprojects.helloworld.domain.features.clients.common.AnimeQuote
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AnimeQuotesVM @Inject constructor(
+    private val repository: IClientRepository<AnimeQuote>
+) : ViewModel() {
+    private val animeQuotesMSF = MutableStateFlow<ConnectionStatus<AnimeQuote>>(ConnectionStatus.Loading(emptyList()))
+    val animeQuotes = animeQuotesMSF.asStateFlow()
+    fun getAnimeQuotes(data: String) {
+        viewModelScope.launch {
+            repository.getData(data).flowOn(Dispatchers.IO).collectLatest {
+                animeQuotesMSF.emit(it)
+            }
+        }
+    }
+}
