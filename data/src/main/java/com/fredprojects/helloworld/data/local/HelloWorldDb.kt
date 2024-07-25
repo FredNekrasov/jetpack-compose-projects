6727fff7c2da3dd55444ed80228a513bb70927ae
FredNekrasov <frednekrasov49@gmail,com>
2024-07-25 10:55:52 +0300

    adding comments to the HelloWorldDb.kt

--- data/src/main/java/com/fredprojects/helloworld/data/local/HelloWorldDb.kt
+++ data/src/main/java/com/fredprojects/helloworld/data/local/HelloWorldDb.kt
@@ -5,6 +5,20 @@ import androidx.room.RoomDatabase
 import com.fredprojects.helloworld.data.local.dao.*
 import com.fredprojects.helloworld.data.local.entities.*
 
+/**
+ * The HelloWorldDb is used to store the data for the app.
+ * The database contains the following entities:
+ *  - [AQEntity]
+ *  - [JDEntity]
+ *  - [MathEntity]
+ *  - [PWEntity]
+ *  - [AstronomyInfoEntity]
+ *  @property aqDao the dao for the [AQEntity]. See [IAQDao]
+ *  @property astronomyInfoDao the dao for the [AstronomyInfoEntity]. See [IAstronomyInfoDao]
+ *  @property mathDao the dao for the [MathEntity]. See [IMathDao]
+ *  @property jdDao the dao for the [JDEntity]. See [IJDDao]
+ *  @property pwDao the dao for the [PWEntity]. See [IPWDao]
+ */
 @Database(
     entities = [PWEntity::class, JDEntity::class, MathEntity::class, AQEntity::class, AstronomyInfoEntity::class],
     version = 1,
