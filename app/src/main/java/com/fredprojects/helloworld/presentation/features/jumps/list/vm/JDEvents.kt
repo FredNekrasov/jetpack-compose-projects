f9f883fc4ed324529ad6d3236a721f66a2c5960f
FredNekrasov <frednekrasov49@gmail,com>
2024-07-25 10:48:28 +0300

    adding comments to events (PWEvents.kt, JDEvents.kt and JREvents.kt) of some ViewModels (PWListVM, JDListVM and JumpingRopeVM)

--- app/src/main/java/com/fredprojects/helloworld/presentation/features/jumps/list/vm/JDEvents.kt
+++ app/src/main/java/com/fredprojects/helloworld/presentation/features/jumps/list/vm/JDEvents.kt
@@ -3,6 +3,15 @@ package com.fredprojects.helloworld.presentation.features.jumps.list.vm
 import com.fredprojects.helloworld.domain.core.utils.SortType
 import com.fredprojects.helloworld.domain.features.jumps.models.JumpData
 
+/**
+ * JDEvents is used to handle events in the JumpDataList
+ * @property Sort the sort type of the list that should be sorted in ascending or descending order by date
+ * @property DeleteJD the data that should be deleted
+ * @property UpsertJD the data that should be inserted or updated
+ * @property GetJD the id of the data that should be edited
+ * @property ToggleSortSection the visibility of the sorting section
+ * @property SwitchingDialog the visibility of the dialog
+ */
 sealed class JDEvents {
     data class Sort(val sortType: SortType) : JDEvents()
     data class DeleteJD(val jumpData: JumpData) : JDEvents()
--- app/src/main/java/com/fredprojects/helloworld/presentation/features/pws/vm/PWEvents.kt
+++ app/src/main/java/com/fredprojects/helloworld/presentation/features/pws/vm/PWEvents.kt
@@ -3,6 +3,14 @@ package com.fredprojects.helloworld.presentation.features.pws.vm
 import com.fredprojects.helloworld.domain.features.pws.models.PracticalWork
 import com.fredprojects.helloworld.domain.features.pws.utils.SortingPW
 
+/**
+ * PWEvents is used to send events to the PWListVM to perform actions
+ * @property Sort used to sort the data by column
+ * @property DeletePW used to delete the practical work from the database
+ * @property SearchPW used to search for the practical work
+ * @property RestorePW used to restore the deleted practical work
+ * @property ToggleSortSection used to switch the visibility of the sorting section
+ */
 sealed class PWEvents {
     data class Sort(val sortingPW: SortingPW) : PWEvents()
     data class DeletePW(val pw: PracticalWork) : PWEvents()
