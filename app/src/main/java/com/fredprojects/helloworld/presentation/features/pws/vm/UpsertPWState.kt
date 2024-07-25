aec6ebd05b36fb8d79b7c3db9d5a89648780cea0
FredNekrasov <frednekrasov49@gmail,com>
2024-07-25 10:50:17 +0300

    adding comments to states (PWState.kt, UpsertPWState.kt, JDState.kt and JRState.kt) of some ViewModels (PWListVM, UpsertPWVM, JDListVM and JumpingRopeVM)

--- app/src/main/java/com/fredprojects/helloworld/presentation/features/pws/vm/UpsertPWState.kt
+++ app/src/main/java/com/fredprojects/helloworld/presentation/features/pws/vm/UpsertPWState.kt
@@ -3,6 +3,11 @@ package com.fredprojects.helloworld.presentation.features.pws.vm
 import com.fredprojects.helloworld.domain.features.pws.models.PracticalWork
 import com.fredprojects.helloworld.domain.features.pws.utils.PWStatus
 
+/**
+ * UpsertPWState is used to insert or update a PracticalWork and its status after the operation is completed
+ * @param pw the PracticalWork to be inserted or updated
+ * @param status the status of the practical work to be inserted or updated
+ */
 data class UpsertPWState(
     val pw: PracticalWork? = null,
     val status: PWStatus = PWStatus.SUCCESS
