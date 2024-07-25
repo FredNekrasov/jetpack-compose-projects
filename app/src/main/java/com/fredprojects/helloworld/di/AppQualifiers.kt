1239ff1d217c2ae32a1903e4fe2aecf65b970d06
FredNekrasov <frednekrasov49@gmail,com>
2024-07-25 12:18:42 +0300

    creating an AppQualifiers to define the components of the domain module and the components of the presentation module

---
+++ app/src/main/java/com/fredprojects/helloworld/di/AppQualifiers.kt
@@ -0,0 +1,18 @@
+package com.fredprojects.helloworld.di
+
+object AppQualifiers {
+    // use cases
+    const val INEQUALITY_USE_CASE = "inequalityUseCase"
+    const val FIBONACCI_USE_CASE = "fibonacciUseCase"
+    const val PW_USE_CASE = "pwUseCases"
+    const val JD_USE_CASE = "jdUseCases"
+    // view models
+    const val PW_LIST_VM = "pwListVM"
+    const val UPSERT_PW_VM = "upsertPWVM"
+    const val JD_LIST_VM = "jdListVM"
+    const val JUMPING_ROPE_VM = "jumpingRopeVM"
+    const val INEQUALITY_VM = "inequalityVM"
+    const val MATH_VM = "mathVM"
+    const val ANIME_QUOTES_VM = "animeQuotesVM"
+    const val ASTRONOMY_INFO_VM = "astronomyInfoVM"
+}
