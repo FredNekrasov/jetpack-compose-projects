e7cdbdd8ceb465f155cacc8aee2ffd6eb3726991
FredNekrasov <frednekrasov49@gmail,com>
2024-07-25 07:49:14 +0300

    removing the hilt plugin from build.gradle.kts (project). adding the compose compiler to the project build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
-    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.jetbrains.kotlin.compose) apply false
     alias(libs.plugins.ksp) apply false
 }
