c89343c4dd44aae74b072266e9bfbd26a7aab9df
FredNekrasov <frednekrasov49@gmail,com>
2024-07-25 16:52:29 +0300

    creating MainEntryPoint.kt and MainScaffold.kt files

---
+++ app/src/main/java/com/fredprojects/helloworld/ui/navigation/MainScaffold.kt
@@ -0,0 +1,31 @@
+package com.fredprojects.helloworld.ui.navigation
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.fredprojects.helloworld.presentation.core.Action
+import com.fredprojects.helloworld.presentation.core.FredTopBar
+
+@Composable
+fun MainScaffold(
+    controller: NavHostController,
+    openDrawer: Action,
+    content: @Composable (PaddingValues, SnackbarHostState) -> Unit
+) {
+    val destination = controller.currentBackStackEntryAsState().value?.destination
+    val snackbarHostState by rememberSaveable { mutableStateOf(SnackbarHostState()) }
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            FredTopBar(
+                condition = (destination?.route != null) && (destination.parent?.startDestinationRoute != destination.route),
+                openDrawer
+            ) { controller.navigateUp() }
+        },
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+    ) { content(it, snackbarHostState) }
+}
