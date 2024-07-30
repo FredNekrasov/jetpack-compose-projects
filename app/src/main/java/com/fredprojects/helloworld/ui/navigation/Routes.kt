9794fe248b2982f812853ee4b4c8f8da38bfd1b9
FredNekrasov <frednekrasov49@gmail.com>
2024-07-30 15:22:26 +0300

    editing the Routes.kt object

--- app/src/main/java/com/fredprojects/helloworld/ui/navigation/Routes.kt
+++ app/src/main/java/com/fredprojects/helloworld/ui/navigation/Routes.kt
@@ -1,13 +1,14 @@
 package com.fredprojects.helloworld.ui.navigation
 
 object Routes {
-    const val INEQUALITY = "inequality screen"
-    const val FIBONACCI = "fibonacci screen"
-    const val PW_LIST = "pw list screen"
-    const val UPSERT_PW = "upsert pw screen"
-    const val JD_LIST = "jumps screen"
-    const val JUMPING_ROPE = "jumping rope screen"
-    const val ASTRONOMY = "astronomy screen"
-    const val MATH = "math screen"
-    val navItems = listOf(INEQUALITY, FIBONACCI, PW_LIST, UPSERT_PW, JD_LIST, JUMPING_ROPE, ASTRONOMY, MATH)
+    private const val INEQUALITY = "inequality"
+    const val FIBONACCI = "fibonacci"
+    private const val PW_LIST = "list of practical works"
+    const val UPSERT_PW = "upsert a practical work"
+    private const val JD_LIST = "jumps"
+    const val JUMPING_ROPE = "jumping rope"
+    private const val ASTRONOMY = "astronomy information"
+    private const val MATH = "math"
+    const val MAIN_SCREEN = "main screen"
+    val mainScreenItems = listOf(FIBONACCI, INEQUALITY, PW_LIST, JD_LIST, ASTRONOMY, MATH)
 }
