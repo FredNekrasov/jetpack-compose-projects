32f84e5fff0dfe210151d14db8dfd29e5d33f8d2
FredNekrasov <frednekrasov49@gmail.com>
2024-07-30 15:35:18 +0300

    added work with the accelerometer sensor to calculate the number of jumps

--- app/src/main/java/com/fredprojects/helloworld/MainActivity.kt
+++ app/src/main/java/com/fredprojects/helloworld/MainActivity.kt
@@ -3,77 +3,112 @@ package com.fredprojects.helloworld
 import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.content.*
-import android.content.pm.PackageManager
-import android.os.*
+import android.hardware.*
+import android.os.Build
+import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.core.content.FileProvider
-import com.fredprojects.helloworld.presentation.R as PR
-import com.fredprojects.helloworld.presentation.core.displayToast
-import com.fredprojects.helloworld.presentation.features.fibonacci.*
-import com.fredprojects.helloworld.ui.navigation.MainEntryPoint
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.lifecycleScope
+import com.fredprojects.helloworld.di.AppQualifiers
+import com.fredprojects.helloworld.domain.features.jumps.utils.JumpStatus
+import com.fredprojects.helloworld.presentation.core.FredButton
+import com.fredprojects.helloworld.presentation.core.FredText
+import com.fredprojects.helloworld.presentation.features.jumps.JumpingRopeDialog
+import com.fredprojects.helloworld.presentation.features.jumps.vm.JumpingRopeVM
+import com.fredprojects.helloworld.ui.navigation.HWNavHost
 import com.fredprojects.helloworld.ui.theme.HelloWorldTheme
-import java.io.File
-import java.util.UUID
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import org.koin.androidx.compose.koinViewModel
+import org.koin.core.qualifier.named
+import kotlin.math.sqrt
 
 class MainActivity : ComponentActivity() {
-    private var fibSequences = listOf<FibonacciBinder>()
+    private val sensorManager by lazy { getSystemService(SENSOR_SERVICE) as SensorManager }
+    private val accelerometer by lazy { sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) }
+    private val xMSF = MutableStateFlow(0f)
+    private val yMSF = MutableStateFlow(0f)
+    private val zMSF = MutableStateFlow(0f)
+    private var accelerationCurrent = 0f
+    private var accelerationLast = 0f
+    private val accelerometerEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent?) {
+            if(event == null) return
+            lifecycleScope.launch {
+                xMSF.emit(event.values[0])
+                yMSF.emit(event.values[1])
+                zMSF.emit(event.values[2])
+            }
+        }
+        override fun onAccuracyChanged(p0: Sensor?, p1: Int) = Unit
+    }
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        createNotificationChannel()
-        val launcher = registerForActivityResult(ActivityResultContracts.TakePicture()) { if(!it) displayToast(getString(PR.string.error)) }
-        val takePicture = {
-            val file = File(filesDir, "fred${UUID.randomUUID()}.jpg")
-            val uri = FileProvider.getUriForFile(this, "fred.provider", file)
-            if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) launcher.launch(uri)
-            uri
-        }
-        val permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
-            takePicture()
-            sendResult("4")
-        }
-        permissionLauncher.launch(arrayOf(Manifest.permission.CAMERA, Manifest.permission.POST_NOTIFICATIONS))
-        enableEdgeToEdge()
-        setContent {
-            HelloWorldTheme {
-                var fibonacciSequences by rememberSaveable { mutableStateOf(fibSequences) }
-                MainEntryPoint(takePicture) {
-                    if(intent.action == DISPLAY_RESULT) {
-                        AnswerFibScreen(intent.getStringExtra(FibSequenceService.RESULT).toString())
-                    } else FibScreen(fibonacciSequences) { fibonacciSequences = sendResult(it) }
-                }
-            }
-        }
-    }
-    private fun createNotificationChannel() {
         val channel = NotificationChannel(CHANNEL_ID, getString(R.string.fibMessageHeader), NotificationManager.IMPORTANCE_DEFAULT)
         channel.description = getString(R.string.fibMessageDescription)
         val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.createNotificationChannel(channel)
-    }
-    private fun sendResult(number: String): List<FibonacciBinder> {
-        val intent = Intent(this, FibSequenceService::class.java)
-        intent.action = UUID.randomUUID().toString()
-        intent.putExtra(FibSequenceService.NUMBER, number.toIntOrNull() ?: 4)
-        val serviceConnection = object : ServiceConnection {
-            override fun onServiceConnected(p0: ComponentName?, bind: IBinder?) {
-                val localBinder = bind as? FibonacciBinder
-                if (localBinder != null) fibSequences += listOf(localBinder)
-            }
-            override fun onServiceDisconnected(p0: ComponentName?) = displayToast(getString(R.string.fibServiceError))
+        val permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+            // todo auth
         }
-        bindService(intent, serviceConnection, BIND_AUTO_CREATE)
-        return fibSequences
+        permissionLauncher.launch(arrayOf(Manifest.permission.CAMERA, Manifest.permission.POST_NOTIFICATIONS))
+        sensorManager.registerListener(accelerometerEventListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+        setContent {
+            HelloWorldTheme {
+                Surface(Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    val jumpingRopeVM: JumpingRopeVM = koinViewModel(named(AppQualifiers.JUMPING_ROPE_VM))
+                    HWNavHost(activityContext = this) { goBack ->
+                        val x = xMSF.collectAsState().value
+                        val y = yMSF.collectAsState().value
+                        val z = zMSF.collectAsState().value
+                        var isShowDialog by rememberSaveable { mutableStateOf(false) }
+                        var numberOfJumps by rememberSaveable { mutableIntStateOf(0) }
+                        Column(Modifier.fillMaxSize(), Arrangement.Center, Alignment.CenterHorizontally) {
+                            val alpha = 0.8f
+                            val gravity = sqrt((alpha * x * x + alpha * y * y + alpha * z * z))
+                            accelerationLast = accelerationCurrent
+                            accelerationCurrent = z - gravity
+                            val diff = accelerationCurrent - accelerationLast
+                            if (diff > 6) numberOfJumps++
+                            FredText("x: $x")
+                            FredText("y: $y")
+                            FredText("z: $z")
+                            FredText("${stringResource(com.fredprojects.helloworld.presentation.R.string.count)}: $numberOfJumps")
+                            FredButton({ jumpingRopeVM.insertJD(numberOfJumps) }, stringResource(com.fredprojects.helloworld.presentation.R.string.save))
+                            FredButton({ isShowDialog = !isShowDialog }, stringResource(com.fredprojects.helloworld.presentation.R.string.jump))
+                            LaunchedEffect(key1 = true) {
+                                jumpingRopeVM.jrState.collectLatest { if(it == JumpStatus.SUCCESS) goBack() }
+                            }
+                        }
+                        if (isShowDialog) JumpingRopeDialog(numberOfJumps) { isShowDialog = !isShowDialog }
+                    }
+                }
+            }
+        }
+    }
+    override fun onStop() {
+        super.onStop()
+        sensorManager.unregisterListener(accelerometerEventListener)
     }
     companion object {
         const val DISPLAY_RESULT = "display_result"
         const val CHANNEL_ID = "fred49"
     }
 }
+/* getting information about sensors in your mobile device
+    sensorManager.getSensorList(Sensor.TYPE_ALL).forEach {
+        Log.d("fred", "name: ${it.name}, type: ${it.stringType}, vendor: ${it.vendor}, version: ${it.version}")
+    }
+*/
