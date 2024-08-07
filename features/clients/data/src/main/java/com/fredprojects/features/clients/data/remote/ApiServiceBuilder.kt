cc15edc65ffc9038fee5f94babbb644c0fe60bd0
FredNekrasov <frednekrasov49@gmail.com>
2024-08-07 23:54:38 +0300

    moving the remote folder from the `data` module to the `features:clients:data` module

--- features/clients/data/src/main/java/com/fredprojects/features/clients/data/remote/ApiServiceBuilder.kt
+++ features/clients/data/src/main/java/com/fredprojects/features/clients/data/remote/ApiServiceBuilder.kt
@@ -1,6 +1,7 @@
-package com.fredprojects.helloworld.data.remote
+package com.fredprojects.features.clients.data.remote
 
-import com.fredprojects.helloworld.data.remote.services.*
+import com.fredprojects.features.clients.data.remote.services.IAstronomyInfoService
+import com.fredprojects.features.clients.data.remote.services.IMathService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -11,5 +12,6 @@ private fun provideRetrofit(baseUrl: String): Retrofit = Retrofit.Builder()
     .baseUrl(baseUrl)
     .addConverterFactory(GsonConverterFactory.create())
     .build()
-fun createAstronomyInfoService(): IAstronomyInfoService = provideRetrofit(IAstronomyInfoService.BASE_URL).create(IAstronomyInfoService::class.java)
+fun createAstronomyInfoService(): IAstronomyInfoService = provideRetrofit(IAstronomyInfoService.BASE_URL).create(
+    IAstronomyInfoService::class.java)
 fun createMathService(): IMathService = provideRetrofit(IMathService.BASE_URL).create(IMathService::class.java)
