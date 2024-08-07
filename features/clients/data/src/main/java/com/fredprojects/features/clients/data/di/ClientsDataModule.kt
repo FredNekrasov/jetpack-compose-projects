2a5823df52d355edb32efb101f3252e682b2a2b6
FredNekrasov <frednekrasov49@gmail.com>
2024-08-08 00:05:38 +0300

    moving the DataSourceModule.kt file from the `data` module to the `features:clients:data` module, renaming it to the ClientsDataModule and refactoring it

---
+++ features/clients/data/src/main/java/com/fredprojects/features/clients/data/di/ClientsDataModule.kt
@@ -0,0 +1,32 @@
+package com.fredprojects.features.clients.data.di
+
+import com.fredprojects.core.database.dao.IAstronomyInfoDao
+import com.fredprojects.core.database.dao.IMathDao
+import com.fredprojects.features.clients.data.remote.*
+import com.fredprojects.features.clients.data.remote.services.*
+import com.fredprojects.features.clients.data.repositories.*
+import com.fredprojects.features.clients.domain.astronomy.repository.IAstronomyRepository
+import com.fredprojects.features.clients.domain.math.repository.IMathRepository
+import org.koin.core.qualifier.qualifier
+import org.koin.dsl.module
+
+val clientsDataModule = module {
+    single(qualifier<IMathService>(), createdAtStart = true) {
+        createMathService()
+    }
+    single(qualifier<IAstronomyInfoService>(), createdAtStart = true) {
+        createAstronomyInfoService()
+    }
+    single(qualifier<IMathRepository>()) {
+        MathRepository(
+            dao = get(qualifier<IMathDao>()),
+            api = get(qualifier<IMathService>())
+        )
+    }
+    single(qualifier<IAstronomyRepository>()) {
+        AstronomyInfoRepository(
+            dao = get(qualifier<IAstronomyInfoDao>()),
+            api = get(qualifier<IAstronomyInfoService>())
+        )
+    }
+}
