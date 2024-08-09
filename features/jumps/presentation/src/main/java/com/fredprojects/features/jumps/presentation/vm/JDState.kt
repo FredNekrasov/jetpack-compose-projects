a5fa6d49a416bc6ef960b7e96074896467b206e9
FredNekrasov <frednekrasov49@gmail.com>
2024-08-09 07:55:21 +0300

    adding a new parameter to the JDState.kt class and removing the jdStatus parameter from there

--- features/jumps/presentation/src/main/java/com/fredprojects/features/jumps/presentation/vm/JDState.kt
+++ features/jumps/presentation/src/main/java/com/fredprojects/features/jumps/presentation/vm/JDState.kt
@@ -8,14 +8,14 @@ import com.fredprojects.features.jumps.domain.utils.SortType
  * JDState is used to store data of the jumps
  * @param jds the list of jump data that will be displayed in the list
  * @param jd the jump data that will be displayed in the dialog and will be edited
- * @param jdStatus the status of the jump data
  * @param sortType the sort type of the jump data list by date
  * @param isSortingSectionVisible the visibility of the sorting section
+ * @param isDialogVisible the visibility of the dialog
  */
 data class JDState(
     val jds: List<JumpData> = emptyList(),
     val jd: JumpData? = null,
-    val jdStatus: JumpStatus = JumpStatus.NOTHING,
     val sortType: SortType = SortType.Descending,
-    val isSortingSectionVisible: Boolean = false
+    val isSortingSectionVisible: Boolean = false,
+    val isDialogVisible: Boolean = false
 )
