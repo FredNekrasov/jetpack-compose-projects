4ca7a2f06e5e7a36e8d8e10aac1be455b42073ea
FredNekrasov <frednekrasov49@gmail.com>
2024-08-09 19:52:57 +0300

    creating models

---
+++ features/clients/domain/src/main/java/com/fredprojects/features/clients/domain/bybit/models/BBType.kt
@@ -0,0 +1,11 @@
+package com.fredprojects.features.clients.domain.bybit.models
+
+/**
+ * BybitType data class used to store product type
+ * @param key the key of the product type
+ * @param title the title of the product type
+ */
+data class BBType(
+    val key: String,
+    val title: String
+)
