9566d76ecc6f2579928cc344aa3d1de701899c6a
FredNekrasov <frednekrasov49@gmail.com>
2024-08-09 10:01:26 +0300

    changing the scope of functions

--- features/clients/presentation/src/main/java/com/fredprojects/features/clients/presentation/Extensions.kt
+++ features/clients/presentation/src/main/java/com/fredprojects/features/clients/presentation/Extensions.kt
@@ -3,7 +3,7 @@ package com.fredprojects.features.clients.presentation
 import com.fredprojects.core.ui.R
 import com.fredprojects.features.clients.domain.utils.ActionStatus
 
-fun ActionStatus.getString() = when(this) {
+internal fun ActionStatus.getString() = when(this) {
     ActionStatus.LOADING -> R.string.wait
     ActionStatus.SUCCESS -> R.string.success
     ActionStatus.CONNECTION_ERROR -> R.string.connectionError
--- features/pws/data/src/main/java/com/fredprojects/features/pws/data/mappers/PWMapper.kt
+++ features/pws/data/src/main/java/com/fredprojects/features/pws/data/mappers/PWMapper.kt
@@ -3,9 +3,9 @@ package com.fredprojects.features.pws.data.mappers
 import com.fredprojects.core.database.entities.PWEntity
 import com.fredprojects.features.pws.domain.models.PracticalWork
 
-fun PracticalWork.toEntity() = PWEntity(
+internal fun PracticalWork.toEntity() = PWEntity(
     pwName, student, variant, level, date, mark, image, id
 )
-fun PWEntity.toDomain() = PracticalWork(
+internal fun PWEntity.toDomain() = PracticalWork(
     pwName, student, variant, level, date, mark, image, id
 )
