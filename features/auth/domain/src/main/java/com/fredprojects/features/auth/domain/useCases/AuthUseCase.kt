c702f410b939a00efb6e7577181e12af9e3b3351
FredNekrasov <frednekrasov49@gmail.com>
2024-08-10 13:03:39 +0300

    adding comments for use cases and refactoring the UpsertUserUseCase

--- features/auth/domain/src/main/java/com/fredprojects/features/auth/domain/useCases/AuthUseCase.kt
+++ features/auth/domain/src/main/java/com/fredprojects/features/auth/domain/useCases/AuthUseCase.kt
@@ -4,9 +4,19 @@ import com.fredprojects.features.auth.domain.models.User
 import com.fredprojects.features.auth.domain.repository.IUserRepository
 import com.fredprojects.features.auth.domain.utils.*
 
+/**
+ * AuthUseCase is used to authenticate a User
+ * @param repository is the repository used to authenticate the User
+ */
 class AuthUseCase(
     private val repository: IUserRepository
 ) {
+    /**
+     * invoke is used to authenticate a User
+     * @param login is the login of the User
+     * @param password is the password of the User
+     * @return Pair<AuthStatus, User?>
+     */
     suspend operator fun invoke(login: String, password: String): Pair<AuthStatus, User?> {
         return when {
             !isUserNameValid(login) -> AuthStatus.INVALID_USERNAME to null
--- features/auth/domain/src/main/java/com/fredprojects/features/auth/domain/useCases/DeleteUserUseCase.kt
+++ features/auth/domain/src/main/java/com/fredprojects/features/auth/domain/useCases/DeleteUserUseCase.kt
@@ -2,8 +2,16 @@ package com.fredprojects.features.auth.domain.useCases
 
 import com.fredprojects.features.auth.domain.repository.IUserRepository
 
+/**
+ * DeleteUserUseCase is used to delete data in the database
+ * @param repository the repository used to delete data
+ */
 class DeleteUserUseCase(
     private val repository: IUserRepository
 ) {
+    /**
+     * invoke is used to delete data in the database
+     * @param userId the id of the user to be deleted
+     */
     suspend operator fun invoke(userId: Int) = repository.delete(userId)
 }
