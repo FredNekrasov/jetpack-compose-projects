671a2f809192637811b86aadec61325d1369b09d
FredNekrasov <frednekrasov49@gmail.com>
2024-08-10 10:16:21 +0300

    changing the DI clientsDataModule for IBBService.kt, BBTypeParser.kt and BBRepository.kt files

--- features/clients/data/src/main/java/com/fredprojects/features/clients/data/di/ClientsDataModule.kt
+++ features/clients/data/src/main/java/com/fredprojects/features/clients/data/di/ClientsDataModule.kt
@@ -1,12 +1,14 @@
 package com.fredprojects.features.clients.data.di
 
-import com.fredprojects.core.database.dao.IAstronomyInfoDao
-import com.fredprojects.core.database.dao.IMathDao
+import com.fredprojects.core.database.dao.*
+import com.fredprojects.features.clients.data.mappers.BBTypeParser
 import com.fredprojects.features.clients.data.remote.*
 import com.fredprojects.features.clients.data.remote.services.*
 import com.fredprojects.features.clients.data.repositories.*
 import com.fredprojects.features.clients.domain.astronomy.repository.IAstronomyRepository
+import com.fredprojects.features.clients.domain.bybit.repository.IBBRepository
 import com.fredprojects.features.clients.domain.math.repository.IMathRepository
+import com.google.gson.Gson
 import org.koin.core.qualifier.qualifier
 import org.koin.dsl.module
 
@@ -16,6 +18,9 @@ val clientsDataModule = module {
     }
     single(qualifier<IAstronomyInfoService>(), createdAtStart = true) {
         createAstronomyInfoService()
+    }
+    single(qualifier<IBBService>(), createdAtStart = true) {
+        createBBService()
     }
     single<IMathRepository>(qualifier<IMathRepository>()) {
         MathRepository(
@@ -27,6 +32,16 @@ val clientsDataModule = module {
         AstronomyInfoRepository(
             dao = get(qualifier<IAstronomyInfoDao>()),
             api = get(qualifier<IAstronomyInfoService>())
+        )
+    }
+    single(qualifier<BBTypeParser>(), createdAtStart = true) {
+        BBTypeParser(Gson())
+    }
+    single<IBBRepository>(qualifier<IBBRepository>()) {
+        BBRepository(
+            dao = get(qualifier<IBBDao>()),
+            api = get(qualifier<IBBService>()),
+            bbTypeParser = get(qualifier<BBTypeParser>())
         )
     }
 }
