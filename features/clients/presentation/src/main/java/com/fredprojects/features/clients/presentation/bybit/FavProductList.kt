6667820413b73105860684c7d297706092b0ae40
FredNekrasov <frednekrasov49@gmail.com>
2024-08-11 14:09:29 +0300

    creating a FavProductList screen

---
+++ features/clients/presentation/src/main/java/com/fredprojects/features/clients/presentation/bybit/FavProductList.kt
@@ -0,0 +1,44 @@
+package com.fredprojects.features.clients.presentation.bybit
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.fredprojects.core.ui.*
+import com.fredprojects.core.ui.R
+import com.fredprojects.features.clients.domain.bybit.models.BBInfo
+
+@Composable
+fun FavProductList(
+    list: List<BBInfo>, onUpdate: (BBInfo) -> Unit, goBack: Action
+) {
+    Scaffold(topBar = { FredTopBar(false, goBack) }) { innerPadding ->
+        Column(Modifier.fillMaxSize().padding(innerPadding), Arrangement.Center, Alignment.CenterHorizontally) {
+            FredHeaderText(stringResource(R.string.shop), MaterialTheme.typography.h5)
+            Spacer(Modifier.height(8.dp))
+            LazyColumn(Modifier.fillMaxSize()) {
+                items(list) { bbInfo ->
+                    if(!bbInfo.favorite) return@items
+                    var isFavorite by remember { mutableStateOf(bbInfo.favorite) }
+                    ProductListItem(
+                        bbInfo = bbInfo,
+                        isFavorite = isFavorite,
+                        onFavouriteChange = {
+                            isFavorite = it
+                            bbInfo.favorite = it
+                            onUpdate(bbInfo)
+                        },
+                        modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp)
+                    )
+                    Spacer(Modifier.height(2.dp))
+                }
+            }
+        }
+    }
+}
