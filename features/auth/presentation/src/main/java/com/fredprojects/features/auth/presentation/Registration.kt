cc4ecb5fc2f883cde8be8832995e2fe199386d80
FredNekrasov <frednekrasov49@gmail.com>
2024-08-11 10:23:45 +0300

    creating a Registration screen

---
+++ features/auth/presentation/src/main/java/com/fredprojects/features/auth/presentation/Registration.kt
@@ -0,0 +1,55 @@
+package com.fredprojects.features.auth.presentation
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.fredprojects.core.ui.*
+import com.fredprojects.core.ui.R
+import com.fredprojects.features.auth.domain.models.User
+import com.fredprojects.features.auth.presentation.vm.AuthEvents
+
+@Composable
+fun Registration(
+    isDataCorrect: Boolean,
+    onRegistration: (AuthEvents) -> Unit,
+    goBack: Action,
+    isRegistration: Boolean
+) {
+    var userName by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    var email by rememberSaveable { mutableStateOf("") }
+    var name by rememberSaveable { mutableStateOf("") }
+    var surname by rememberSaveable { mutableStateOf("") }
+    Column(Modifier.fillMaxSize(),Arrangement.Center, Alignment.CenterHorizontally) {
+        FredHeaderText(
+            if(isRegistration) stringResource(R.string.registration) else stringResource(R.string.editingData),
+            MaterialTheme.typography.h5
+        )
+        Spacer(Modifier.height(32.dp))
+        FredTextField(userName, { userName = it }, R.string.enterUN, isDataCorrect)
+        if(!isDataCorrect) FredText(stringResource(R.string.incorrectUserName), color = MaterialTheme.colors.error)
+        Spacer(Modifier.height(4.dp))
+        PasswordTextField(value = password, onChange = { password = it }, isValueCorrect = isDataCorrect)
+        if(!isDataCorrect) FredText(stringResource(R.string.incorrectPassword), color = MaterialTheme.colors.error)
+        Spacer(Modifier.height(4.dp))
+        FredTextField(email, { email = it }, R.string.enterEmail, isDataCorrect, keyboardType = KeyboardType.Email)
+        if(!isDataCorrect) FredText(stringResource(R.string.incorrectEmail), color = MaterialTheme.colors.error)
+        Spacer(Modifier.height(4.dp))
+        FredTextField(name, { name = it }, R.string.enterName)
+        Spacer(Modifier.height(4.dp))
+        FredTextField(surname, { surname = it }, R.string.enterSurname)
+        Spacer(Modifier.height(16.dp))
+        FredButton(
+            { onRegistration(AuthEvents.UpsertUserData(User(userName, password, email, name, surname))) },
+            if(isRegistration) stringResource(R.string.signUp) else stringResource(R.string.save)
+        )
+        Spacer(Modifier.height(4.dp))
+        FredButton(goBack, stringResource(R.string.goBack))
+    }
+}
