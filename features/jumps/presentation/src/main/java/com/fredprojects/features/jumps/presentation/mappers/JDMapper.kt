0e7d25d3f10e09f3c29aa4e13a05d6a63413ab5b
FredNekrasov <frednekrasov49@gmail.com>
2024-08-11 20:48:06 +0300

    creating a JDMapper to convert from a presentation model to a domain model and vice versa

---
+++ features/jumps/presentation/src/main/java/com/fredprojects/features/jumps/presentation/mappers/JDMapper.kt
@@ -0,0 +1,11 @@
+package com.fredprojects.features.jumps.presentation.mappers
+
+import com.fredprojects.features.jumps.domain.models.JumpData
+import com.fredprojects.features.jumps.presentation.models.JDPModel
+
+internal fun JumpData.toPresentation() = JDPModel(
+    count, date, id
+)
+internal fun JDPModel.toDomain() = JumpData(
+    count, date, id
+)
