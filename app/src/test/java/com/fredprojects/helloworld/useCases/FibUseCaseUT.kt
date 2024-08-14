06fedae2223b31955343ae731655364ab82c2172
FredNekrasov <frednekrasov49@gmail.com>
2024-08-14 13:21:29 +0300

    creating unit tests for the FibonacciUseCase

---
+++ app/src/test/java/com/fredprojects/helloworld/useCases/FibUseCaseUT.kt
@@ -0,0 +1,38 @@
+package com.fredprojects.helloworld.useCases
+
+import com.fredprojects.features.fibonacci.api.useCases.FibonacciUseCase
+import com.fredprojects.features.fibonacci.api.utils.CalculationStatus.*
+import junit.framework.TestCase.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.test.*
+import org.junit.*
+import java.math.BigInteger
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class FibUseCaseUT {
+    private val fibUseCase = FibonacciUseCase()
+    // correct inputs
+    @Test
+    fun solutionIsError(): Unit = runTest(UnconfinedTestDispatcher()) {
+        val result = fibUseCase(-1)
+        assertEquals(result, Pair(ERROR, BigInteger.ZERO))
+    }
+    @Test
+    fun solutionIsSuccess(): Unit = runTest(UnconfinedTestDispatcher()) {
+        val result = fibUseCase(1)
+        val expected = 3
+        assertEquals(result, Pair(SUCCESS, expected.toBigInteger()))
+    }
+    // incorrect inputs
+    @Test
+    fun solutionIsError2(): Unit = runTest(UnconfinedTestDispatcher()) {
+        val result = fibUseCase(0)
+        assertNotSame(result, Pair(ERROR, BigInteger.ZERO))
+    }
+    @Test
+    fun solutionIsSuccess2(): Unit = runTest(UnconfinedTestDispatcher()) {
+        val result = fibUseCase(-1)
+        val expected = -3
+        assertNotSame(result, Pair(SUCCESS, expected.toBigInteger()))
+    }
+}
