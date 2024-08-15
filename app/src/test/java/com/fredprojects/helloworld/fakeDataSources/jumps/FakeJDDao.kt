3cd6f0976d1eed93a59d9e0c0f026250bd38dca2
FredNekrasov <frednekrasov49@gmail.com>
2024-08-15 09:39:51 +0300

    creating the FakeJDDao for unit tests

---
+++ app/src/test/java/com/fredprojects/helloworld/fakeDataSources/jumps/FakeJDDao.kt
@@ -0,0 +1,17 @@
+package com.fredprojects.helloworld.fakeDataSources.jumps
+
+import com.fredprojects.core.database.dao.IJDDao
+import com.fredprojects.core.database.entities.JDEntity
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class FakeJDDao: IJDDao {
+    private val jdList = mutableListOf<JDEntity>()
+    override suspend fun upsert(jdEntity: JDEntity) {
+        if(jdEntity.id == null) jdList.add(jdEntity) else jdList[jdEntity.id!!] = jdEntity
+    }
+    override suspend fun delete(jdEntity: JDEntity) {
+        jdList.remove(jdEntity)
+    }
+    override fun getAll(): Flow<List<JDEntity>> = flow { emit(jdList) }
+}
