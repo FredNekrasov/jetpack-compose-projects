f8e8cf821c0383d53f10ef797d5e8f2c86adab11
FredNekrasov <frednekrasov49@gmail.com>
2024-08-15 09:40:22 +0300

    creating the FakePWDao for unit tests

---
+++ app/src/test/java/com/fredprojects/helloworld/fakeDataSources/pws/FakePWDao.kt
@@ -0,0 +1,18 @@
+package com.fredprojects.helloworld.fakeDataSources.pws
+
+import com.fredprojects.core.database.dao.IPWDao
+import com.fredprojects.core.database.entities.PWEntity
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class FakePWDao : IPWDao {
+    private val pwList = mutableListOf<PWEntity>()
+    override suspend fun upsert(pwEntity: PWEntity) {
+        if(pwEntity.id == null) pwList.add(pwEntity) else pwList[pwEntity.id!!] = pwEntity
+    }
+    override suspend fun delete(pwEntity: PWEntity) {
+        pwList.remove(pwEntity)
+    }
+    override fun getAll(): Flow<List<PWEntity>> = flow { emit(pwList) }
+    override suspend fun getById(id: Int): PWEntity? = pwList.firstOrNull { it.id == id }
+}
