91abf37bba259478db134f6a2dd2a8f37f4ed518
FredNekrasov <frednekrasov49@gmail.com>
2024-08-15 09:39:02 +0300

    creating the FakeAstronomyInfoService for unit tests

---
+++ app/src/test/java/com/fredprojects/helloworld/fakeDataSources/astronomy/FakeAstronomyInfoService.kt
@@ -0,0 +1,23 @@
+package com.fredprojects.helloworld.fakeDataSources.astronomy
+
+import com.fredprojects.features.clients.data.remote.dto.AstronomyInfoDto
+import com.fredprojects.features.clients.data.remote.services.IAstronomyInfoService
+
+class FakeAstronomyInfoService : IAstronomyInfoService {
+    override suspend fun getAstronomyInfo(
+        ra: String, dec: String, radius: Float
+    ): Map<String, AstronomyInfoDto>? {
+        if(radius == 2f) return null
+        return mapOf(
+            "test1" to AstronomyInfoDto(references = listOf("test"), name = listOf("test1")),
+            "test2" to AstronomyInfoDto(references = listOf("test"), name = listOf("test2")),
+            "test3" to AstronomyInfoDto(references = listOf("test"), name = listOf("test3")),
+            "test4" to AstronomyInfoDto(references = listOf("test"), name = listOf("test4")),
+            "test5" to AstronomyInfoDto(references = listOf("test"), name = listOf("test5")),
+            "test6" to AstronomyInfoDto(references = listOf("test"), name = listOf("test6")),
+            "test7" to AstronomyInfoDto(references = listOf("test"), name = listOf("test7")),
+            "test8" to AstronomyInfoDto(references = listOf("test"), name = listOf("test8")),
+            "test9" to AstronomyInfoDto(references = listOf("test"), name = listOf("test9"))
+        )
+    }
+}
