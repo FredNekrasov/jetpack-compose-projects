88e452db87f102731bc8328aaaab53c19163a32e
FredNekrasov <frednekrasov49@gmail.com>
2024-08-18 10:20:23 +0300

    creating unit tests for the DeletePWUseCase

---
+++ app/src/test/java/com/fredprojects/helloworld/useCases/pws/DeletePWUseCaseUT.kt
@@ -0,0 +1,40 @@
+package com.fredprojects.helloworld.useCases.pws
+
+import com.fredprojects.features.pws.data.repositories.PWRepository
+import com.fredprojects.features.pws.domain.models.PracticalWork
+import com.fredprojects.features.pws.domain.useCases.PWUseCases
+import com.fredprojects.features.pws.domain.useCases.crud.*
+import com.fredprojects.features.pws.domain.utils.*
+import com.fredprojects.helloworld.fakeDataSources.pws.FakePWDao
+import junit.framework.TestCase.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class DeletePWUseCaseUT {
+    private lateinit var pwUseCases: PWUseCases
+    @Before
+    fun setUp() {
+        val fakePWDao = FakePWDao()
+        val pwRepository = PWRepository(fakePWDao)
+        val upsertPWUseCase = UpsertPWUseCase(pwRepository)
+        val getPWUseCase = GetPWUseCase(pwRepository)
+        val deletePWUseCase = DeletePWUseCase(pwRepository)
+        pwUseCases = PWUseCases(getPWUseCase, upsertPWUseCase, deletePWUseCase)
+    }
+    // deletePWUseCase & getPWUseCase
+    @Test
+    fun delete() = runTest(UnconfinedTestDispatcher()) {
+        val pw = PracticalWork("name", "student", 3, 3, "11.11.2022", 5, "image")
+        val result = pwUseCases.upsert(pw)
+        assertEquals(result, PWStatus.SUCCESS)
+        pwUseCases.delete(pw)
+        pwUseCases.getPWs(SortingPW.Date(SortType.Descending)).collectLatest {
+            assert(it.isEmpty())
+        }
+    }
+}
