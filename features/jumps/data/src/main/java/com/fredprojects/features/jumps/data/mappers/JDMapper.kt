8fc87ae9ac945e929dc19caaed3896a1f6714cc5
FredNekrasov <frednekrasov49@gmail.com>
2024-09-04 07:10:41 +0300

    refactoring the JumpData model and its mappers

--- features/jumps/data/src/main/java/com/fredprojects/features/jumps/data/mappers/JDMapper.kt
+++ features/jumps/data/src/main/java/com/fredprojects/features/jumps/data/mappers/JDMapper.kt
@@ -2,29 +2,14 @@ package com.fredprojects.features.jumps.data.mappers
 
 import com.fredprojects.core.database.entities.JDEntity
 import com.fredprojects.features.jumps.domain.models.JumpData
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
-import java.time.format.DateTimeParseException
 
 internal fun JumpData.toEntity() = JDEntity(
     count = count,
-    date = date.toString(),
+    date = date,
     id = id
 )
 internal fun JDEntity.toDomain() = JumpData(
     count = count,
-    date = date.toLocalDate() ?: LocalDate.now(),
+    date = date,
     id = id
 )
-/**
- * toLocalDate is used to convert a string to a LocalDate in the format "yyyy-MM-dd".
- * if the string is not in the format "yyyy-MM-dd" it will return null.
- */
-private fun String.toLocalDate() = try {
-    if (this.contains(Regex("""^\d{4}-\d{2}-\d{2}$"""))) {
-        val localDate = LocalDate.parse(this, DateTimeFormatter.ofPattern("yyyy-MM-dd"))
-        if (localDate < LocalDate.now()) localDate else null
-    } else null
-} catch (e: DateTimeParseException) {
-    null
-}
--- features/jumps/presentation/src/main/java/com/fredprojects/features/jumps/presentation/mappers/JDMapper.kt
+++ features/jumps/presentation/src/main/java/com/fredprojects/features/jumps/presentation/mappers/JDMapper.kt
@@ -2,21 +2,10 @@ package com.fredprojects.features.jumps.presentation.mappers
 
 import com.fredprojects.features.jumps.domain.models.JumpData
 import com.fredprojects.features.jumps.presentation.models.JDPModel
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
-import java.time.format.DateTimeParseException
 
 internal fun JumpData.toPresentation() = JDPModel(
-    count, date.toString(), id
+    count, date, id
 )
 internal fun JDPModel.toDomain() = JumpData(
-    count ?: 0, date.toLocalDate() ?: LocalDate.now(), id
+    count ?: 0, date, id
 )
-private fun String.toLocalDate() = try {
-    if (this.contains(Regex("""^\d{4}-\d{2}-\d{2}$"""))) {
-        val localDate = LocalDate.parse(this, DateTimeFormatter.ofPattern("yyyy-MM-dd"))
-        if (localDate <= LocalDate.now()) localDate else null
-    } else null
-} catch (e: DateTimeParseException) {
-    null
-}
