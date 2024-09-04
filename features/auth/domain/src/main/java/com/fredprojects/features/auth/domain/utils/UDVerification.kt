4fa75fc1a50c474c64dc921feeee8a0e4992c296
FredNekrasov <frednekrasov49@gmail.com>
2024-09-04 07:13:21 +0300

    removing an unnecessary import from the UDVerification.kt file

--- features/auth/domain/src/main/java/com/fredprojects/features/auth/domain/utils/UDVerification.kt
+++ features/auth/domain/src/main/java/com/fredprojects/features/auth/domain/utils/UDVerification.kt
@@ -1,7 +1,5 @@
 package com.fredprojects.features.auth.domain.utils
 
-import com.fredprojects.features.auth.domain.models.User
-
 // UserDataVerification
 internal fun isUserNameValid(login: String): Boolean = login.isNotBlank()
 internal fun isPasswordValid(password: String): Boolean = password.isNotBlank() && password.length >= 6
